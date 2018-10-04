0042ad68339954e41d62454c0dc736fbe99471f5
TinyKenny <Jonathan@rawet.se>
2018-10-04 09:13:21 +0200

    continued work on the camera

--- GameJamHT2018/Assets/Scripts/CameraControls.cs
+++ GameJamHT2018/Assets/Scripts/CameraControls.cs
@@ -7,7 +7,12 @@ public class CameraControls : MonoBehaviour {
     public Transform player;
 
     private bool hasPlayer = true;
-    private Vector3 distanceFromPlayer = new Vector3(0.0f, 0.0f, 10.0f);
+    private float distanceFromPlayer = 10.0f;
+    [Range(0.0f, 1.0f)]
+    public float angle = 0.0f;
+
+
+    private Vector3 temp_distanceFromPlayer = new Vector3(0.0f, 0.0f, 10.0f);
 
 	// Use this for initialization
 	void Start () {
@@ -16,14 +21,18 @@ public class CameraControls : MonoBehaviour {
             hasPlayer = false;
             Debug.LogError("Camera does not have an object to track");
         }
+        testThingy();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (hasPlayer)
         {
-            transform.position = player.position - distanceFromPlayer;
+            //transform.position = player.position - temp_distanceFromPlayer;
+            transform.position = player.position - new Vector3(0.0f, -distanceFromPlayer * angle, distanceFromPlayer * (1.0f - angle));
+            transform.LookAt(player);
         }
+        Debug.Log(Vector3.Distance(transform.position, player.position));
 	}
 
     void testThingy()
