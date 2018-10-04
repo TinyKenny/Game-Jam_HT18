11067fd499309b6d461cc1e479068b6cb781672e
TinyKenny <Jonathan@rawet.se>
2018-10-04 08:44:28 +0200

    started working on camera-stuff

--- GameJamHT2018/Assets/Scripts/CameraControls.cs
+++ GameJamHT2018/Assets/Scripts/CameraControls.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class CameraControls : MonoBehaviour {
 
+    public Transform player;
+
+    private bool hasPlayer = true;
+    private Vector3 distanceFromPlayer = new Vector3(0.0f, 0.0f, 10.0f);
+
 	// Use this for initialization
 	void Start () {
-		
+		if (player == null)
+        {
+            hasPlayer = false;
+            Debug.LogError("Camera does not have an object to track");
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (hasPlayer)
+        {
+            transform.position = player.position - distanceFromPlayer;
+        }
 	}
+
+    void testThingy()
+    {
+        Debug.Log("Testing!");
+        return;
+    }
 }
---
+++ GameJamHT2018/Assets/Scripts/Player/Player_Temp_Controls.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Temp_Controls : MonoBehaviour { //this only exists to test the camera script
+
+    private float movementSpeed = 5.0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+		if (Mathf.Abs(horizontalMovement) >= 0.0f || Mathf.Abs(verticalMovement) >= 0.0f)
+        {
+            transform.Translate(new Vector3(horizontalMovement, 0.0f, verticalMovement) * movementSpeed * Time.deltaTime);
+        }
+	}
+}
