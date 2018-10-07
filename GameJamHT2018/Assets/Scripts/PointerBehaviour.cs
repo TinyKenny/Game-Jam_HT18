63764c85d2e40e19c140c8a73fe1afe3743268cd
TinyKenny <Jonathan@rawet.se>
2018-10-07 14:24:15 +0200

    pointerstuff

--- GameJamHT2018/Assets/Scripts/PointerBehaviour.cs
+++ GameJamHT2018/Assets/Scripts/PointerBehaviour.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class PointerBehaviour : MonoBehaviour {
 
-    private float OriginalHeight;
-    private float RotationSpeed = 1.0f;
-    private float MovementSpeed = 1.0f;
+    private float RotationSpeed = 150.0f;
+    private float MovementSpeed = 2.0f;
     private float MovementRange = 1.0f;
+    private static float MovementDirection = 1.0f;
+    private static Vector3 Position = new Vector3(0.0f, 0.0f, 0.0f);
+    private static Quaternion Rotation = new Quaternion();
 
 	// Use this for initialization
 	void Start () {
-        OriginalHeight = transform.position.y;
+        UpdatePosition();
+        UpdateRotation();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(1.0f, 0.5f, 0.1f) * RotationSpeed * Time.deltaTime);
-        transform.Translate(new Vector3(0.0f, 1.0f, 0.0f) * MovementSpeed * Time.deltaTime);
+        //transform.Rotate(new Vector3(1.0f, 0.5f, 0.1f) * RotationSpeed * Time.deltaTime);
+        UpdatePosition();
+        UpdateRotation();
 	}
+
+    private void UpdatePosition()
+    {
+        Position += new Vector3(0.0f, 1.0f, 0.0f) * MovementSpeed * MovementDirection * Time.deltaTime;
+        transform.localPosition = Position;
+        if (transform.localPosition.y > MovementRange)
+        {
+            MovementDirection = -1.0f;
+        }
+        else if (transform.localPosition.y < 0.0f)
+        {
+            MovementDirection = 1.0f;
+        }
+    }
+
+    private void UpdateRotation()
+    {
+        transform.rotation = Rotation;
+        transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f) * RotationSpeed * Time.deltaTime);
+        Rotation = transform.rotation;
+    }
 }
